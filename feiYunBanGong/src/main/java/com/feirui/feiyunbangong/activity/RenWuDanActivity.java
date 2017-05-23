262504c55d9d9cc14af1b3cba30eb13f8e0ade23
lice <450098886@qq.com>
2017-05-23 14:04:25 +0800

    Adding files

---
+++ feiYunBanGong/src/main/java/com/feirui/feiyunbangong/activity/RenWuDanActivity.java
@@ -0,0 +1,15 @@
+package com.feirui.feiyunbangong.activity;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import com.feirui.feiyunbangong.R;
+
+public class RenWuDanActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ren_wu_dan);
+    }
+}
