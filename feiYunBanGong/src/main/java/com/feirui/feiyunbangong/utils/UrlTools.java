06ff3cd6395dd5b053cccc9708619453d160aa00
feirui <rnd@feirui.org>
2017-09-28 18:18:29 +0800

    so库

--- feiYunBanGong/src/main/java/com/feirui/feiyunbangong/utils/UrlTools.java
+++ feiYunBanGong/src/main/java/com/feirui/feiyunbangong/utils/UrlTools.java
@@ -20,8 +20,8 @@ public class UrlTools {
   /**
    * 链接地址
    */
-  public static String url = "http://123.57.45.74/feiybg/public/index.php/api/";
-  public static String url1 = "http://123.57.45.74/feiybg/public/index.php/index/";
+  public static String url = "http://123.57.45.74/feiybg1/public/index.php/api/";
+  public static String url1 = "http://123.57.45.74/feiybg1/public/index.php/index/";
 //public static String url = "http://175.25.23.139/feiybg/public/index.php/api/";
 // public static String fyUrl = "http://175.25.23.139/feiybg/public/index.php/index/";
 
